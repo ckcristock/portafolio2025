cfa70373700ad9234601fc2d2232523d5982a2a9
Cristian Montoya <ckcristock@gmail.com>
2025-07-18 07:55:37 -0500

    create formgroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Subject } from 'rxjs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular';
+  observable = new Subject<string>();
+  form;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [''],
+    });
+  }
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 }
