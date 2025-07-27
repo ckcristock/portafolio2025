2a144831d4bc4abd8583b83e0526b5166eee89b1
Cristian Montoya <ckcristock@gmail.com>
2025-07-26 22:30:55 -0500

    install angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,28 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular';
+
+  products: string[] = [
+    'Camiseta de Equipo A',
+    'Balón de Fútbol Nike',
+    'Botas de Fútbol Adidas',
+    'Guantes de Portero Puma',
+    'Shorts de Entrenamiento',
+    'Medias de Fútbol',
+    'Espilleras Nike',
+    'Chaqueta de Chándal',
+    'Mochila Deportiva',
+    'Gorra de Equipo B',
+    'Balón de Baloncesto', // Añadido para mostrar que no todos los productos son de fútbol
+    'Camiseta de Equipo C',
+    'Balón de Voleibol',
+  ];
+
+  filteredProducts: string[] = [];
+
+  constructor() {}
 }
