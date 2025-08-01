bc783d513a9a9a872fc9404fbc78139045a86e93
Cristian Montoya <ckcristock@gmail.com>
2025-07-31 19:32:58 -0500

    create two main components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { InputComponent } from './components/input/input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonsComponent, InputComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular';
